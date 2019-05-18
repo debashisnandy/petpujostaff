52d9ab355e058819b0600f55b012f8c4bb9747b8
Debashis Nandy <31581078+debashisnandy0111@users.noreply.github.com>
2019-05-18 19:48:12 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyD4H1VoQkAm7wDwKmQamTca54uwBhKyW68",
-    authDomain: "fir-demoapp-567ff.firebaseapp.com",
-    databaseURL: "https://fir-demoapp-567ff.firebaseio.com",
-    projectId: "fir-demoapp-567ff",
-    storageBucket: "fir-demoapp-567ff.appspot.com",
-    messagingSenderId: "216843675802"
-  }
+  
 };
