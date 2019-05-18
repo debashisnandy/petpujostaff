fc1f451aa5f8c255e098394a16c9dbdd66147f6d
Debashis Nandy <31581078+debashisnandy0111@users.noreply.github.com>
2019-05-18 19:47:43 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyD4H1VoQkAm7wDwKmQamTca54uwBhKyW68",
-    authDomain: "fir-demoapp-567ff.firebaseapp.com",
-    databaseURL: "https://fir-demoapp-567ff.firebaseio.com",
-    projectId: "fir-demoapp-567ff",
-    storageBucket: "fir-demoapp-567ff.appspot.com",
-    messagingSenderId: "216843675802"
-  }
+  
 };
 
 /*
